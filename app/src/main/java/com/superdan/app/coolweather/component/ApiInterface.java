b3236a99f0a33581651eb087607bd0730e67dd23
dsz <364615401@qq.com>
2016-03-15 23:06:59 +0800

    1.和风天气的接口代码

---
+++ app/src/main/java/com/superdan/app/coolweather/component/ApiInterface.java
@@ -0,0 +1,24 @@
+package com.superdan.app.coolweather.component;
+
+import junit.runner.Version;
+
+import retrofit.http.GET;
+import retrofit.http.Query;
+import rx.Observable;
+
+/**
+ * Created by dsz on 16/3/15.
+ */
+public interface ApiInterface {
+    String HOST="https://api.heweather.com/x3/";
+    @GET("weather")
+    Observable<WeatherAPI>mWeatherAPI(@Query("city")String city,@Query("key")String key);
+
+
+    //在Retrofit2.0中 我们还可以在@Url里面定义完整的URL：这种情况下Base URl会被忽略。
+
+    @GET("http://api.fir.im/apps/latest/5630e5f1f2fc425c52000006")
+    Observable<VersionAPI> mVersionAPI(@Query("api_token")String api_token);
+
+
+}
--- app/src/main/java/com/superdan/app/coolweather/modules/domain/Weather.java
+++ app/src/main/java/com/superdan/app/coolweather/modules/domain/Weather.java
@@ -1,9 +1,29 @@
 package com.superdan.app.coolweather.modules.domain;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 /**
- * Created by Administrator on 2016/3/15.
+ * Created by 段胜泽 on 2016/3/15.
  */
+//为什么要实现Serializable
 public class Weather implements Serializable {
+    /**
+     * "basic": {
+     "city": "大连",
+     "cnty": "中国",
+     "id": "CN101070201",
+     "lat": "38.944000",
+     "lon": "121.576000",
+         "update": {
+         "loc": "2015-07-15 10:43",
+         "utc": "2015-07-15 02:46:14"
+         }
+     }
+     */
+    @SerializedName("")
+
+
+
 }
